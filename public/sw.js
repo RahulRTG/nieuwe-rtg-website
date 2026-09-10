d0d7b781df55d42b3c8e365f96b33841586923bf
RahulRTG <roellie.i@gmail.com>
2026-09-10 17:23:41 +0200

    Voorkom een mutatielus in Uw ruimte
    (cherry picked from commit 5e70c75c19bea41b33a01aaec1808539e6ead749)

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,7 @@
    installeerde in de periode dat de `cache: 'no-cache'` hieronder was
    gesneuveld (zie de toelichting daar). Een naam die uit de INHOUD komt kan
    niet vergeten worden. */
-const CACHE = 'rtg-app-44b91540';
+const CACHE = 'rtg-app-b1bc6774';
 const SHELL = ['/apps/app.html', '/shared/id.js',
   /* Heritage is één systeemlaag. Een offline start mag niet alleen de HTML
      bewaren en daarna identiteit, materiaal, beweging of lettertypen missen. */
